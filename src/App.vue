f5268d77b6c19bf063e4d27204ef5368ecc0e819
akoruth95 <akoruth95@gmail.com>
2018-09-26 20:11:27 -0400

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app>
+      <v-navigation-drawer v-model="sidebar" app>
+      </v-navigation-drawer>
+
+      <v-toolbar app>
+        <span class="hidden-sm-up">
+          <v-toolbar-side-icon @click="sidebar = !sidebar">
+          </v-toolbar-side-icon>
+        </span>
+        <v-toolbar-title>{{ appTitle }}</v-toolbar-title>
+        <v-spacer></v-spacer>
+      </v-toolbar>
+
+      <v-content>
+        <router-view></router-view>
+      </v-content>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        appTitle: 'USO Event Manager',
+        sidebar: false
+      }
+    }
+  }
+</script>
