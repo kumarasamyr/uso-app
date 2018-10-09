a96c3afc4a3486c1696989e76599fbaaba2f4ddc
ausaif@gmail.com <ausaif@gmail.com>
2018-10-09 00:12:31 -0400

    Added scaffolding

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,8 @@
       dark
       app
     >
-      <v-btn :color="(fab!='home')?'grey':'white'" :fab="fab=='home'" light :icon="fab!='home'"  @click.stop="fab = 'home'">
+      <v-spacer></v-spacer>
+      <v-btn to="/" :color="(fab!='home')?'grey':'white'" :fab="fab=='home'" light :icon="fab!='home'"  @click.stop="fab = 'home'">
         <v-badge right color="red">
           <v-icon color="secondary">fa-home</v-icon>
         </v-badge>
@@ -14,85 +15,48 @@
       <v-btn :color="(fab!='globe')?'grey':'white'" :fab="fab=='globe'" light :icon="fab!='globe'"  @click.stop="fab = 'globe'">
         <v-icon color="secondary">fas fa-globe</v-icon>
       </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>fas fa-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title"></v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-spacer></v-spacer>
+      <img class="" height="55px" src="./assets/uso-logo.svg"/>
       <v-spacer></v-spacer>
     </v-toolbar>
     <v-content color="secondary">
-      <router-view/>
+      <transition name="fade">
+        <router-view/>
+      </transition>
     </v-content>
-    
-    <v-card>
-    <v-bottom-nav
-      :active.sync="bottomNav"
-      :value="true"
-      color="secondary"
-      absolute
-      app
-    >
-      <v-btn dark>
-        <span>Bookmarks</span>
-        <v-icon>fa-bookmark</v-icon>
-      </v-btn>
-
-      <v-btn dark>
-        <span>Notifications</span>
-        <v-badge fab color="primary">
-          <span slot="badge">99</span>
-          <v-icon>fa-bell</v-icon>
-        </v-badge>
-      </v-btn>
-
-      <v-btn dark>
-        <span>Games</span>
-        <v-icon>fa-gamepad</v-icon>
-      </v-btn>
-
-      <v-btn dark>
-        <span>Settings</span>
-        <v-icon>fa-user-cog</v-icon>
-      </v-btn>
-
-    </v-bottom-nav>
-  </v-card>
-
+    <bottom-bar></bottom-bar>
   </v-app>
 </template>
 
 <script>
-
+import bottomBar from "./components/BottomBar"
 export default {
   name: 'App',
   data () {
     return {
       fab: "home",
-      clipped: false,
-      drawer: true,
-      fixed: false,
-      items: [{
-        icon: 'fas fa-shapes',
-        title: 'Inspire'
-      }],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js',
       bottomNav: 0
     }
   },
   computed: {
-    color () {
-      switch (this.bottomNav) {
-        case 0: return 'primary'
-        case 1: return 'primary'
-        case 2: return 'brown'
-        case 3: return 'indigo'
-      }
-    }
+  },
+  components: {
+    bottomBar
   }
 }
 </script>
 <style scoped>
+.fade-enter-active, .fade-leave-active {
+  transition-property: opacity;
+  transition-duration: .25s;
+}
+
+.fade-enter-active {
+  transition-delay: .25s;
+}
+
+.fade-enter, .fade-leave-active {
+  opacity: 0
+}
 </style>
---
+++ src/components/BottomBar.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-card>
+    <v-bottom-nav
+      :active.sync="bottomNav"
+      :value="true"
+      color="secondary"
+      absolute
+      app
+    >
+      <v-btn dark to="/bookmark">
+        <span>Bookmarks</span>
+        <v-icon>fa-bookmark</v-icon>
+      </v-btn>
+
+      <v-btn dark to="/notification">
+        <span>Notifications</span>
+        <v-badge fab color="primary">
+          <span slot="badge">99</span>
+          <v-icon>fa-bell</v-icon>
+        </v-badge>
+      </v-btn>
+
+      <v-btn dark to="/game">
+        <span>Games</span>
+        <v-icon>fa-gamepad</v-icon>
+      </v-btn>
+
+      <v-btn dark to="/setting">
+        <span>Settings</span>
+        <v-icon>fa-user-cog</v-icon>
+      </v-btn>
+    </v-bottom-nav>
+  </v-card> 
+</template>
+<script>
+export default {
+  data () {
+    return {
+      bottomNav: 0
+    }
+  }
+}
+</script>
+
