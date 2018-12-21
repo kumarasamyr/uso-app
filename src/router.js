7fcb95268451cbc3263793c9f1f352062a815238
ausaif@gmail.com <ausaif@gmail.com>
2018-12-21 16:32:34 -0500

    fixed router

--- src/router.js
+++ src/router.js
@@ -1,79 +1,79 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './pages/Home.vue'
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "./pages/Home.vue";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: Home
     },
     {
-      path: '/login',
-      name: 'login',
-      component: () => import('./pages/Login.vue')
+      path: "/login",
+      name: "login",
+      component: () => import("./pages/Login.vue")
     },
     {
-      path: '/bookmark',
-      name: 'bookmark',
+      path: "/bookmark",
+      name: "bookmark",
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "bookmark" */ './pages/Bookmark.vue')
+      component: () =>
+        import(/* webpackChunkName: "bookmark" */ "./pages/Bookmark.vue")
     },
     {
-      path: '/game',
-      name: 'game',
-      component: () => import('./pages/Game.vue')
+      path: "/game",
+      name: "game",
+      component: () => import("./pages/Game.vue")
     },
     {
-      path: '/notification',
-      name: 'notification',
-      component: () => import('./pages/Notification.vue')
+      path: "/notification",
+      name: "notification",
+      component: () => import("./pages/Notification.vue")
     },
     {
-      path: '/setting',
-      name: 'setting',
-      component: () => import('./pages/Setting.vue')
+      path: "/setting",
+      name: "setting",
+      component: () => import("./pages/Setting.vue")
     },
     {
-      path: '/agenda',
-      name: 'agenda',
-      component: () => import('./pages/Session/Agenda/Agenda.vue')
+      path: "/agenda",
+      name: "agenda",
+      component: () => import("./pages/Session/Agenda/Agenda.vue")
     },
     {
-      path: '/session-info',
-      name: 'session-info',
-      component: () => import('./pages/Session/SessionInfo/SessionInfo.vue')
+      path: "/session-info",
+      name: "session-info",
+      component: () => import("./pages/Session/SessionInfo/SessionInfo.vue")
     },
     {
-      path: '/my-events',
-      name: 'my-events',
-      component: () => import('./pages/events/EventList.vue')
+      path: "/my-events",
+      name: "my-events",
+      component: () => import("./pages/Events/EventList.vue")
     },
     {
-      path: '/events',
-      name: 'events',
-      component: () => import('./pages/events/EventsHome.vue')
+      path: "/events",
+      name: "events",
+      component: () => import("./pages/Events/EventsHome.vue")
     },
     {
-      path: '/eventdetails/:id',
-      name: 'eventdetails',
-      component: () => import('./pages/events/EventsDetails.vue')
-      },
-    {
-      path: '/events/feedback',
-      name: 'feedback',
-      component: () => import('./pages/events/Feedback.vue')
+      path: "/eventdetails/:id",
+      name: "eventdetails",
+      component: () => import("./pages/Events/EventsDetails.vue")
     },
     {
-      path: '/faq',
-      name: 'faq',
-      component: () => import('./pages/Info/Faq.vue')
+      path: "/events/feedback",
+      name: "feedback",
+      component: () => import("./pages/Events/Feedback.vue")
+    },
+    {
+      path: "/faq",
+      name: "faq",
+      component: () => import("./pages/Info/Faq.vue")
     }
-   
   ]
-})
+});
