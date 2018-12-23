2e0ab7beaf73ca0e9e3d162503c06abd203757ec
ausaif@gmail.com <ausaif@gmail.com>
2018-12-23 12:59:21 -0500

    resolve merge conflict

--- src/pages/events/Feedback.vue
+++ src/pages/events/Feedback.vue
@@ -1,40 +1,38 @@
 <template>
   <div>
-    <v-toolbar
-      color="secondary"
-      height="70px"
-      dark
-      app
-    >
-    <div id="title"> <h1>Your Events</h1></div>
+    <v-toolbar color="secondary" height="70px" dark app>
+      <div id="title">
+        <h1>Your Events</h1>
+      </div>
     </v-toolbar>
-    <h1> feedback</h1>
+    <h1>feedback</h1>
     <event-title></event-title>
   </div>
 </template>
 
 <script>
-
-import eventTitle from "../../components/EventTitle"
+import axios from "axios";
+import eventTitle from "../../components/EventTitle";
 
 export default {
   data() {
     return {
       posts: []
-    }
+    };
   },
   created() {
-    axios.get(`http://jsonplaceholder.typicode.com/posts`)
-    .then(response => {
-      // JSON responses are automatically parsed.
-      this.posts = response.data
-    })
-    .catch(e => {
-      this.errors.push(e)
-    })
+    axios
+      .get(`http://jsonplaceholder.typicode.com/posts`)
+      .then(response => {
+        // JSON responses are automatically parsed.
+        this.posts = response.data;
+      })
+      .catch(e => {
+        this.errors.push(e);
+      });
   },
-  components :{
-  	eventTitle
+  components: {
+    eventTitle
   }
-}
+};
 </script>
